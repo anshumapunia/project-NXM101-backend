77c05debb59eb9c3583e36fe421f88ec647ef539
anshumapunia123 <112813318+anshumapunia123@users.noreply.github.com>
2023-05-05 15:58:25 +0530

    project

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const { connection } = require("./data");
 const app = express();
-// const { userRouter } = require("./routes/user.router");
+const { userRouter } = require("./routes/user.router");
 require("dotenv").config();
 
 const cors = require("cors");
 
-// const { productRouter } = require("./routes/product.router");
-const { authenticate } = require("./middleware/middleware.auth");
+const { productRouter } = require("./routes/product.router");
+// const { authenticate } = require("./middleware/middleware.auth");
 app.use(express.json());
 app.use(cors());
 
-// app.use("/users", userRouter);
-app.use(authenticate);
+app.use("/users", userRouter);
+// app.use(authenticate);
 
-// app.use("/product", productRouter);
+app.use("/product", productRouter);
 
 app.listen(process.env.PORT, async () => {
   try {
